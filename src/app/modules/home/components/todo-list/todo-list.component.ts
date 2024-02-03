de6b731650794d2d1c1b2f332a63838206737131
fabiovilela <fabiovilela@ibm.com>
2024-02-03 19:48:14 -0300

    to-do list

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss'],
+  
+})
+export class TodoListComponent {
+
+}
